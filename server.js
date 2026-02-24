89151d74f3d2e97a41599fda6591ba0948bb2838
dissojak <dissojak@gmail.com>
2026-02-24 08:08:13 +0100

    Adem: fixing sonner Qube warrning

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 8000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-  });
+await connectDB();
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
